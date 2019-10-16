2bd314c6c25d81ce0cc7f7641ea9e5a4d5f4c10f
javic <javic@DESKTOP-FONOMVE>
2019-10-16 20:39:43 +0200

    Primer LogIn a medias

---
+++ ProyectoTetris/src/Package/Menu.java
@@ -0,0 +1,95 @@
+package Package;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Menu extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtusuario;
+	private JPasswordField jpassclave;
+	private JButton btnIngresar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Menu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 205);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("LOGIN");
+		lblLogin.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogin.setBounds(172, 13, 56, 16);
+		contentPane.add(lblLogin);
+		
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblUsuario.setBounds(44, 76, 56, 16);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblContrasena = new JLabel("Contrasena:");
+		lblContrasena.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblContrasena.setBounds(44, 116, 90, 16);
+		contentPane.add(lblContrasena);
+		
+		txtusuario = new JTextField();
+		txtusuario.setBounds(142, 73, 116, 22);
+		contentPane.add(txtusuario);
+		txtusuario.setColumns(10);
+		
+		jpassclave = new JPasswordField();
+		jpassclave.setBounds(146, 114, 112, 20);
+		contentPane.add(jpassclave);
+		
+		btnIngresar = new JButton("Ingresar");
+		btnIngresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			/*char[] clave = jpassclave.getPassword();
+				//String claveFinal = new String (clave);
+				
+				if(txtusuario.getText().equals("Javier") && claveFinal.equals("123") ) { 
+			dispose();
+			JOptionPane*/
+				}
+		});
+		btnIngresar.setBounds(288, 72, 97, 25);
+		contentPane.add(btnIngresar);
+	}
+}
